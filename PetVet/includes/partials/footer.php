c2d8cbc89ae0785fcdcd857baeeafc593470cc90
Julie Quinty <julie.quinty@hotmail.fr>
2023-02-10 11:42:51 +0100

    Ajout du formulaire de contact & des mentions légales

--- PetVet/includes/partials/footer.php
+++ PetVet/includes/partials/footer.php
@@ -2,14 +2,14 @@
     <ul class ="footer-menu">
             <h3 class = "footer-title">Liens Rapides</h3>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="">Contact</a></li>
+            <li><a href="includes/core/views/formContact.phtml">Contact</a></li>
             <li><a href="includes/core/views/formConnexion.phtml">Connexion/Inscription</a></li>
     </ul>
     <ul class ="footer-menu">
             <h3 class = "footer-title">Informations Légales</h3>
-            <li><a href="mentionLegales.php">Mention Légales</a></li>
-            <li><a href="cgu.php">Conditions Générales d'Utilisation</a></li>
-            <li><a href="rgpd.php">Politique de Confidentialité</a></li>
+            <li><a href="/public/informationsLegales/mentionLegales.php">Mention Légales</a></li>
+            <li><a href="/public/informationsLegales/mentionLegales.php">Conditions Générales d'Utilisation</a></li>
+            <li><a href="/public/informationsLegales/mentionLegales.php">Politique de Confidentialité</a></li>
     </ul>
 </div>
 <div class ="copyright">
--- PetVet/includes/partials/navbarNoCo.php
+++ PetVet/includes/partials/navbarNoCo.php
@@ -3,7 +3,7 @@
 </a>-->
 <img class="logo" src="public/images/Logo_Site_PetVet.png" alt="logo du site">
 <a href="index.php">Accueil</a>
-<a href="">Contact</a>
+<a href="includes/core/views/formContact.phtml">Contact</a>
 <a href="includes/core/views/formConnexion.phtml">Connexion/Inscription</a>
 <!--<div id="btnBurger">
     <button><i class="fa-solid fa-bars"></i></button>
